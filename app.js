69a432ce67cf9847b017b884326270e42ddcf3b4
Muhammad Farooq <irfankhans786787788@gmail.com>
2024-10-31 23:09:47 +0500

    bug:picture issue fixed

--- app.js
+++ app.js
@@ -1,53 +1,49 @@
-// window.localStorage.setItem('fname','AB Waqas');
-// var Fname = window.localStorage.getItem('fname');
-// console.log(Fname)
 function submitData() {
+  let stdName = document.getElementById("stdName").value;
+  let stdFatherName = document.getElementById("stdFatherName").value;
+  let stdClass = document.getElementById("stdClass").value;
+  let stdId = document.getElementById("stdId").value;
+  let stdSection = document.getElementById("stdSection").value;
+  let stdPicture = document.getElementById("stdPicture").files[0]; // Get the file object
 
-let stdName = document.getElementById("stdName").value;
-let stdFatherName = document.getElementById("stdFatherName").value;
-let stdClass = document.getElementById("stdClass").value;
-let stdId = document.getElementById("stdId").value;
-let stdSection = document.getElementById("stdSection").value;
-let stdPicture = document.getElementById("stdPicture").value;
+  console.log("pic =", stdPicture);
 
-// console.log('pic =',stdPicture);
-// console.log('name =',stdName);
-// console.log('Fname =',stdFatherName);
-// console.log('class =',stdClass);
-// console.log('sec =',stdSection);
-// console.log('id =',stdId);
+  // Save other data to localStorage
+  window.localStorage.setItem("stdName", stdName);
+  window.localStorage.setItem("stdFatherName", stdFatherName);
+  window.localStorage.setItem("stdClass", stdClass);
+  window.localStorage.setItem("stdId", stdId);
+  window.localStorage.setItem("stdSection", stdSection);
 
-var f = window.localStorage.setItem('stdName',stdName);
-window.localStorage.setItem('stdFatherName',stdFatherName);
-window.localStorage.setItem('stdClass',stdClass);
-window.localStorage.setItem('stdId',stdId);
-window.localStorage.setItem('stdSection',stdSection);
-window.localStorage.setItem('stdPicture',stdPicture);
+  // Handle the picture file
+  if (stdPicture) {
+    let pictureUrl = URL.createObjectURL(stdPicture); // Create a URL for the file
+    window.localStorage.setItem("stdPicture", pictureUrl); // Store the URL in localStorage
+  }
 
+  // Update the displayed information
+  let showStdName = document.getElementById("showStdName");
+  let showStdFatherName = document.getElementById("showStdFatherName");
+  let showStdClass = document.getElementById("showStdClass");
+  let showStdId = document.getElementById("showStdId");
+  let showStdSection = document.getElementById("showStdSection");
+  let showStdPicture = document.getElementById("showStdPicture");
 
-let showStdName = document.getElementById("showStdName");
-let showStdFatherName = document.getElementById("showStdFatherName");
-let showStdClass = document.getElementById("showStdClass");
-let showStdId = document.getElementById("showStdId");
-let showStdSection = document.getElementById("showStdSection");
-let showStdPicture = document.getElementById("showStdPicture");
+  showStdName.innerHTML = window.localStorage.getItem("stdName");
+  showStdFatherName.innerHTML = window.localStorage.getItem("stdFatherName");
+  showStdId.innerHTML = window.localStorage.getItem("stdId");
+  showStdClass.innerHTML = window.localStorage.getItem("stdClass");
+  showStdSection.innerHTML = window.localStorage.getItem("stdSection");
 
-showStdName.innerHTML = window.localStorage.getItem('stdName'); 
-//variable k name se nhe Get kr sakty ('' is k through hi krty hn)
-showStdFatherName.innerHTML = window.localStorage.getItem('stdFatherName');
-showStdId.innerHTML = window.localStorage.getItem('stdId');
-showStdClass.innerHTML = window.localStorage.getItem('stdClass');
-showStdSection.innerHTML = window.localStorage.getItem('stdSection');
-// showStdPicture.innerHTML = window.localStorage.getItem('stdPicture');
-showStdPicture.style.backgroundImage = "url(window.localStorage.getItem('stdPicture')"
+  // Set the background image for the picture element
+  showStdPicture.style.backgroundImage = `url(${window.localStorage.getItem(
+    "stdPicture"
+  )})`;
 
-
-// alert('Your Form have Submited, Now Generate the ID Card');
-
+  // Optional: Alert message
+  // alert('Your Form has been submitted, Now generate the ID Card');
 }
 
 function showCard() {
-
-    document.querySelector('.id-card').className += ' show';
-
+  document.querySelector(".id-card").className += " show";
 }
